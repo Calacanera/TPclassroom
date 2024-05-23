016c243f77113940ae67c3c29781d2e8efff5626
Juan Mendez <juanlmendez>
2024-05-23 11:00:24 -0300

    Corrigiendo error de texto en materias

--- js/script.js
+++ js/script.js
@@ -29,3 +29,42 @@ function updateProgressBar() {
 window.onscroll = function() {
    updateProgressBar();
 };
+
+//Materias
+function mostrarTema() {
+   var select = document.getElementById("materias");
+   var matematicas = document.getElementById("matematicas1");
+   var quimica = document.getElementById("quimica1");
+   var fisica = document.getElementById("fisica1");
+ 
+   if (select.value === "matematicas") {
+     matematicas.style.display = "block";
+    
+   } else {
+     matematicas.style.display = "none";
+   }
+
+   if (select.value === "quimica") {
+      quimica.style.display = "block";
+     
+    } else {
+      quimica.style.display = "none";
+    }
+  
+    if (select.value === "fisica") {
+      fisica.style.display = "block";
+     
+    } else {
+      fisica.style.display = "none";
+    }
+
+   //  if (select.value === "fisica") {
+   //    tema.style.display = "block";
+   //    sintesis.style.display = "block";
+   //    video.style.display = "block";
+   //  } else {
+   //    tema.style.display = "none";
+   //    sintesis.style.display = "none";
+   //    video.style.display = "none";
+   //  }
+ }
